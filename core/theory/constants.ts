843376cc337026673cfc1b8ab2fbe0f287bc222d
jhkim000601@naver.com <jhkim000601@naver.com>
2026-04-17 19:11:32 +0900

    feat: 기타 종류 추가, 악기 라벨명 수정

--- core/theory/constants.ts
+++ core/theory/constants.ts
@@ -18,20 +18,34 @@ export type InstrumentConfig = {
 export const INSTRUMENTS: InstrumentConfig[] = [
   {
     id: "guitar_6",
-    label: "Guitar (6현)",
+    label: "기타 6현",
     strings: [40, 45, 50, 55, 59, 64], // E2 A2 D3 G3 B3 E4
     fretCount: 15,
   },
   {
+    id: "guitar_7",
+    label: "기타 7현",
+    strings: [35, 40, 45, 50, 55, 59, 64], // B1 E2 A2 D3 G3 B3 E4
+    fretCount: 15,
+  },
+  {
     id: "bass_4",
-    label: "Bass (4현)",
+    label: "베이스 4현",
     strings: [28, 33, 38, 43], // E1 A1 D2 G2
     fretCount: 15,
   },
-  // 나중에 추가:
-  // { id: "guitar_7", label: "Guitar (7현)", strings: [35, 40, 45, 50, 55, 59, 64], fretCount: 15 },
-  // { id: "bass_5",   label: "Bass (5현)",   strings: [23, 28, 33, 38, 43], fretCount: 15 },
-  // { id: "bass_6",   label: "Bass (6현)",   strings: [23, 28, 33, 38, 43, 47], fretCount: 15 },
+  {
+    id: "bass_5",
+    label: "베이스 5현",
+    strings: [23, 28, 33, 38, 43], // B0 E1 A1 D2 G2
+    fretCount: 15,
+  },
+  {
+    id: "bass_6",
+    label: "베이스 6현",
+    strings: [23, 28, 33, 38, 43, 47], // B0 E1 A1 D2 G2 C3
+    fretCount: 15,
+  },
 ];
 
 // 지판 포지션 마커 (홀수 프렛 점, 12프렛 복점)
